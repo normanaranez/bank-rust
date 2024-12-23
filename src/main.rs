5714fb8e8334bc9a522b43936b388e22480c0e59
Norman Aranez <araneznorman12@gmail.com>
2024-12-23 10:44:50 +0800

    first commit

---
+++ src/main.rs
@@ -0,0 +1,70 @@
+#[derive(Debug)]
+struct Account {
+    id: u32,
+    balance: i32,
+    holder: String,
+}
+
+#[derive(Debug)]
+struct Bank {
+    accounts: Vec<Account>,
+}
+
+impl Bank {
+    fn new() -> Self {
+        Bank { accounts: vec![]}
+    }
+    fn add_account(&mut self, account: Account) {
+        self.accounts.push(account);
+    }
+    fn total_balance(&self) -> i32 {
+        self.accounts.iter().fold(0, |acc, account| acc + account.balance)
+    }
+    fn summary(&self) -> Vec<String> {
+        self.accounts
+            .iter()
+            .map(|account| account.summary())
+            .collect::<Vec<String>>()
+    }
+}
+
+impl Account {
+    fn new(id: u32, holder: String) -> Self {
+        Account { 
+            id, 
+            holder,
+            balance: 0,
+        }
+    }
+    fn deposit(&mut self, amount: i32)-> i32 {
+        self.balance += amount;
+        self.balance
+    }
+
+    fn withdraw(&mut self, amount: i32)-> i32 {
+        self.balance -= amount;
+        self.balance
+    }
+
+    fn summary(&self) -> String {
+        format!("Account {} is held by {} with balance {}", self.id, self.holder, self.balance)
+    }
+}
+
+fn main() {
+
+    let mut bank = Bank::new();
+
+    let mut account = Account::new(1, "Alice".to_string());
+
+    account.deposit(100);
+
+    account.withdraw(50);
+
+    bank.add_account(account);
+    
+    println!("{:#?}", bank.summary());
+
+    println!("Total balance: {}", bank.total_balance());
+
+}
